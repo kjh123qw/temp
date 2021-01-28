3418fba3aa9597e4e19e183421e49e07c98706f9
JHKIM <kjh123qwa@gmail.com>
2021-01-29 01:45:02 +0900

    commit

--- JobSatellite/settings.py
+++ JobSatellite/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'recommend.apps.RecommendConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recommend'
 ]
 
 MIDDLEWARE = [
--- recommend/urls.py
+++ recommend/urls.py
@@ -5,5 +5,4 @@ from . import views
 app_name = 'recommend'
 urlpatterns = [
     path('', views.index, name='index'),
-
 ]
