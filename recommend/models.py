2cb4075941354706eeb3fc287289b84387752944
hyeri <hyeri2565@skku.edu>
2021-01-29 19:01:48 +0900

    8000server test completed & each test page made.

--- recommend/models.py
+++ recommend/models.py
@@ -11,10 +11,10 @@ from django.db import models
 #recruit_info에 포린키 추가함
 #jobid는 어떻게?
 class recruit_info(models.Model):
-    companyId=models.ForeignKey("company",related_name='companyid',on_delete=models.CASCADE,db_column='companyId')
+    companyId=models.IntegerField(default=0)
     title=models.CharField(max_length=30)
     #csv에선 job아이디이지만 해당공고 타이틀에 대한 id이므로 titleId로 설정함
-    titleId=models.IntegerField()
+    titleId=models.IntegerField(default=0)
     job=models.CharField(max_length=30)
     #직무 추천시 사용되는 역량 4가지 skill, task, require, prefer
     skill=models.CharField(max_length=50)
@@ -24,28 +24,37 @@ class recruit_info(models.Model):
     jobUrl=models.CharField(max_length=60)
 
 class company(models.Model):
-    companyId=models.IntegerField(primary_key=True)
-    company=models.CharField(max_length=30)
-    reviewCount=models.IntegerField()
-    average=models.IntegerField()
-    salary=models.IntegerField()
-    wlb=models.IntegerField()
-    culture=models.IntegerField()
-    possibility=models.IntegerField()
-    management=models.IntegerField()
+    companyId=models.IntegerField(null=True,default=0)
+    company_name=models.CharField(max_length=30,default='',null=True)
+    reviewCount=models.IntegerField(default=0)
+    average=models.IntegerField(default=0)
+    salary=models.IntegerField(default=0)
+    wlb=models.IntegerField(default=0)
+    culture=models.IntegerField(default=0)
+    possibility=models.IntegerField(default=0)
+    management=models.IntegerField(default=0)
 
 class jobdict(models.Model):
     job=models.CharField(max_length=30)
-    python=models.IntegerField()
-    java=models.IntegerField()
-    javascript=models.IntegerField()
+    python=models.IntegerField(default=0)
+    java=models.IntegerField(default=0)
+    javascript=models.IntegerField(default=0)
 
 #mainpage에 쓰일 스킬 대분류소분류 테이블 필요하면 엑셀에 정리해서 모델링
 #작업하고 사용하기
 
 class skill(models.Model):
-    skillId=models.IntegerField(primary_key=True)
+    skillId=models.IntegerField(default=0)
     skill=models.CharField(max_length=30)
+
+#col=index,jobid,company,title,job,skill,region,experience,intro
+#task,require,prefer,jobUrl
+class test(models.Model):
+    titleId=models.IntegerField(default=0)
+    company=models.CharField(max_length=50)
+    title=models.CharField(max_length=50)
+    job=models.CharField(max_length=50)
+    skill=models.CharField(max_length=50)
 
 
 
--- recommend/views.py
+++ recommend/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 import pandas as pd
+from recommend.models import test
 import os
-# Create your views here.
+
 def index (request):
     return render(request,'index.html')
 def fir_result(request):
@@ -14,3 +15,18 @@ def readCsv(address):
     df=pd.read_csv(address)
     return df
 # address 파라미터 적어줄때  => r'절대주소'
+
+test_df=readCsv(r'C:\Users\네리\Desktop\sba_re\4조\job_data_list_final.csv')
+print(test_df.columns)
+#col=index,jobId,company,title,job,skill,region,experience,intro
+#task,require,prefer,jobUrl
+test_df=test_df.fillna('')
+
+print(type(test_df['jobId'][0]))
+
+#test => titleId,company,title,job,skill
+def create():
+    for i in range(len(test_df)):
+        test.objects.create(titleId=test_df['jobId'][i], company=test_df['company'][i], title=test_df['title'][i], job=test_df['job'][i], skill=test_df['skill'][i])
+print(len(test_df))
+#model.objects.all()
